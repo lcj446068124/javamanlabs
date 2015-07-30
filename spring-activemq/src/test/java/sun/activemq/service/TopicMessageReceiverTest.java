1c111fb73bce74b142a6bed482800bdd49118fc7
sun yamorn <yamorn@outlook.com>
2015-07-30 16:58:52 +0800

    add topic and queue demo

---
+++ spring-activemq/src/test/java/sun/activemq/service/TopicMessageReceiverTest.java
@@ -0,0 +1,24 @@
+package sun.activemq.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by 264929 on 2015/7/30.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:application-context.xml"})
+public class TopicMessageReceiverTest {
+    @Autowired
+    private TopicMessageReceiver topicMessageReceiver;
+
+    @Test
+    public void testReceive() throws Exception {
+        topicMessageReceiver.receive();
+    }
+}
